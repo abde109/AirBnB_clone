33c178198458ff3bd7934d675c2ef62d7a9ef75c
abde109 <abderrahimkhadri2@gmail.com>
2023-10-13 09:44:01 +0100

    add create instence using old one

--- models/base_model.py
+++ models/base_model.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 
-import uuid
+from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
     """This is the BaseModel class"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel instance."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Return the string representation of the BaseModel."""
