9f3b98a1fddb409596b9a105fb1266c3bcf793e0
abde109 <abderrahimkhadri2@gmail.com>
2023-10-14 23:17:41 +0100

    fix fil_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,50 +1,48 @@
 #!/usr/bin/python3
-"""
-This module contains the FileStorage class that allows for CRUD operations on objects.
-"""
+"""FileStorage class handles the serialization and deserialization of instances"""
 from json import loads, dumps
 from os.path import exists
-from models.user import User
-from models.base_model import BaseModel
-
+from ..user import User  # Modified import
+from ..base_model import BaseModel  # Modified import
 
 class FileStorage:
-    """
-    Storage class, allows for CRUD operations on objects.
-    """
+    """Storage class, allows for CRUD operations on objects"""
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """
-        Returns all stored objects.
-        """
+        """Returns all stored objects"""
         return self.__objects
 
     def new(self, obj):
-        """
-        Creates a new object and adds it to the storage.
-        """
+        """Creates new object"""
         attr = "{}.{}".format(type(obj).__name__, obj.id)
-        self.__objects[attr] = obj.to_dict()
+        self.__objects[attr] = obj  # store object instance, not dictionary
 
     def save(self):
-        """
-        Serializes objects to a JSON file.
-        """
+        """Save objects to output.json"""
+        serialized_objects = {}
+        for key, obj in self.__objects.items():
+            serialized_objects[key] = obj.to_dict()  # Call to_dict() to serialize
+
         with open(self.__file_path, "w") as f:
-            json_object = dumps(self.__objects)
+            json_object = dumps(serialized_objects)
             f.write(json_object)
 
     def reload(self):
-        """
-        Deserializes objects from a JSON file.
-        """
+        """Reads from output.json to load objects"""
         current_classes = {'BaseModel': BaseModel, 'User': User}
         if not exists(self.__file_path):
             with open(self.__file_path, "w") as g:
                 g.write("{}")
         with open(self.__file_path, "r") as f:
             data = f.read()
+
         json_decode = loads(data)
-        self.__objects = json_decode
+
+        # Deserialize each object and store it in self.__objects
+        for key, value in json_decode.items():
+            class_name = value['__class__']
+            if class_name in current_classes:
+                self.__objects[key] = current_classes[class_name](**value)
+
