9a1aba483053b819fd927b1fd027206404994630
abde109 <abderrahimkhadri2@gmail.com>
2023-10-15 00:41:25 +0100

    fix condole

--- console.py
+++ console.py
@@ -66,10 +66,15 @@ class HBNBCommand(cmd.Cmd):
         for identifier in list(instances.all()):
             id = identifier.split('.')
             if (id[0] == tofind[0] and id[1] == tofind[1]):
+                obj = instances.all()[identifier]
+                obj_dict = obj.__dict__
+                ordered_keys = ['first_name', 'id', 'created_at', 'updated_at']
+                ordered_dict = {k: obj_dict[k] for k in ordered_keys if k in obj_dict}
 
-                print(instances.all()[identifier])
+                print("[{}] ({}) {}".format(id[0], id[1], ordered_dict))
                 return
         print("** no instance found **")
+
 
     def do_destroy(self, args):
         """remove instance of model based on id"""
