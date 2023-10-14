eefdd422b35503443fe6b7675ce45e21f3d1fa3b
abde109 <abderrahimkhadri2@gmail.com>
2023-10-14 22:57:15 +0100

    fix base_model & fil_storage pycodestyle

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,45 +1,52 @@
 #!/usr/bin/python3
-"""my very first custom backend orm :)"""
+"""
+This module defines the FileStorage class for handling storage of objects.
+"""
+
 from json import loads, dumps
 from os.path import exists
 from models.user import User
 from models.base_model import BaseModel
 
-
 class FileStorage:
-    """"Storage class, allows for crud of objects"""
+    """
+    FileStorage class for serializing and deserializing instances.
+    """
+    
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """returns all stored objects"""
+        """
+        Return all stored objects.
+        """
         return self.__objects
 
     def new(self, obj):
-        """creates new object"""
-        attr = "{}.{}".format(type(obj).__name__, obj.id)
-        self.__objects[attr] = obj.to_dict()
+        """
+        Create a new object.
+        """
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj.to_dict()
 
     def save(self):
-        """save objects to output.json"""
-        f = open(self.__file_path, "w")
-
-        json_object = dumps(self.__objects)
-        with f:
-            f.write(json_object)
-        f.close()
+        """
+        Serialize objects to the JSON file.
+        """
+        with open(self.__file_path, "w") as f:
+            dumps(self.__objects, f)
 
     def reload(self):
-        current_classes = {'BaseModel': BaseModel, 'User': User}
-        """reads from output.json to load objects"""
-        if not exists(self.__file_path):
-            g = open(self.__file_path, "w")
-            g.write("{}")
-            g.close()
-        f = open(self.__file_path, "r")
+        """
+        Deserialize objects from the JSON file.
+        """
+        if exists(self.__file_path):
+            with open(self.__file_path, "r") as f:
+                self.__objects = loads(f.read())
 
-        with f:
-            data = f.read()
-        json_decode = loads(data)
-        self.__objects = json_decode
-        f.close()
+            current_classes = {'BaseModel': BaseModel, 'User': User}
+
+            for key, obj_dict in self.__objects.items():
+                class_name = obj_dict["__class__"]
+                if class_name in current_classes:
+                    self.__objects[key] = current_classes[class_name](**obj_dict)
